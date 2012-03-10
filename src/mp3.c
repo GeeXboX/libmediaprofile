b8bc19676132e499e7a00b098ed21952ce239d57
Benjamin Zores <ben@geexbox.org>
2012-03-10 13:34:59 +0100

    add mp3 parser as an example

---
+++ src/mp3.c
@@ -0,0 +1,16 @@
+#include "mediaprofile_internals.h"
+
+static int
+mp3_parser (media_profile_t *p, FILE *f)
+{
+  printf ("Parsing MP3 file ...\n");
+
+  return MP_PARSER_OK;
+}
+
+mp_parser_t mp_parser_mp3 = {
+  .name       = "MPEG Audio",
+  .type       = MEDIA_PROFILE_TYPE_AUDIO,
+  .extensions = "mpa,mp3",
+  .parse      = mp3_parser,
+};
